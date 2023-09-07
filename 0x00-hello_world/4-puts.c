ac35625911d3fcc25cea2c69fe8f14c66a661ef9
Lillymuller <eleltaalemu@example.com>
2023-09-07 10:27:07 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ *main = main entry point
+ *return = always 0 (success)
+ */
+int main(void)
 {
-char message[]="Programming is like building a multilingual puzzle\n";
-puts(message);
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
