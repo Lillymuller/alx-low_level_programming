ec6e907587882ee641090a600f7ea40005ab2d76
Lillymuller <eleltaalemu@example.com>
2023-09-12 07:57:33 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+void print_alphabet(void);
+{
+for (char abc = 'a'; abc <= 'z'; abc++)
+{
+_putchar(abc);
+}
+_putchar('\n');
+return (0);
+}
