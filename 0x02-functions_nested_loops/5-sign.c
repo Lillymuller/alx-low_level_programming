5ea377ee00e317a8dc3617910db6a4ed56d68779
Lillymuller <eleltaalemu@example.com>
2023-09-12 10:37:59 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-*print_sign - show 1 if a number is greater than 0, show 0 if a number is 0 and show a -1 if a number is less than 0
+ *print_sign - declare if the input is greater, equal or less than zero
 *@n: integer
 * Return: 1 is greater than zero, 0 is zero, -1 less than zero
 */
 int print_sign(int n)
 {
-if (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
