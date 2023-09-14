07e7d7e61d1ee76c3a7fa9a263f7928ae036c0ae
Lillymuller <eleltaalemu@example.com>
2023-09-14 07:56:09 -0700

    100-print_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	unsigned long number = 612852475143;
+		unsigned long prime_factor = 2;
+
+		while (prime_factor <= number)
+		{
+			if (number % prime_factor == 0)
+			{
+				number /= prime_factor;
+				prime_factor = 2;
+			}
+			else
+				prime_factor++;
+		}
+		printf("%lu\n", number);
+		return (0);
+}
