7f1e32a51234997d02404f7d842658d237b0c378
Lillymuller <eleltaalemu@example.com>
2023-09-14 05:41:39 -0700

    6-print_lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *print_line - Draws a stright line
+  *Return: Always 0 (success)
+  **/
+
+void print_line(int n)
+{
+for (; n > 0; n--)
+_putchar('_');
+_putchar('\n');
+}
