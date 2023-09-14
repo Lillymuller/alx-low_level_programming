bd5fbe5e620cdd10325c093f307bb0201dabb443
Lillymuller <eleltaalemu@example.com>
2023-09-14 05:46:49 -0700

    6-print_lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
   *print_line - Draws a stright line
+  *@n: number of lines
   *Return: Always 0 (success)
   **/
 
 void print_line(int n)
 {
-for (; n > 0; n--)
+	int i;
+
+for (i = 0; i < n; i++)
+{
 _putchar('_');
+}
 _putchar('\n');
 }
