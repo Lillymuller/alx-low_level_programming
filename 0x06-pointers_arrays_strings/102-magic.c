525bc51e0b29cde3c71fa1982d8ffc661ecea011
Lillymuller <eleltaalemu@example.com>
2023-09-20 13:22:41 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
-  *main - Entry point
-  *Return: Always 0
-  */
 
 int main(void)
 {
@@ -12,11 +8,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 * - you are not allowed to modify
+	 * - you are not allowed to modify p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
