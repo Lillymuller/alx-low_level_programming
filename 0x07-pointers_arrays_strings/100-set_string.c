ca188c305185e6b8d537ca21930eeb95aae04b24
Lillymuller <eleltaalemu@example.com>
2023-09-25 06:38:00 -0700

    100-set_string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,17 @@
 #include "main.h"
+/**
+  *set_string - set pointer to char
+  *@s: character pointer
+  *@to: character pointer
+  *Return: Always 0
+  */
 
 void set_string(char **s, char *to)
+
 {
-	char **s 
+	char **s = "Bob";
+	char *to = "Robert";
+
+	'B'[0] = *(&B[0]); /* pointer to first element of string literal*/
+	'R'[0] = *(&R[0]); /* pointer to first element of string literal*/
+}
