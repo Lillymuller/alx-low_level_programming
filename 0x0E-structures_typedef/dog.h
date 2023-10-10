c14cd4dca1b69bbb404cec1d423f03697fcdcb05
Lillymuller <eleltaalemu@example.com>
2023-10-10 10:05:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_max;
+typedef struct dog dog_t;
 
 /**
  * struct dog - structure for dogs
@@ -21,7 +21,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_max *new_dog(char *name, float age, char *owner);
-void free_dog(dog_max *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
