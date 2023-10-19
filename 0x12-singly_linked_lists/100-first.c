f2a0df2e00e3918e1d51346496b022a9c931bba2
Lillymuller <eleltaalemu@example.com>
2023-10-19 11:46:08 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * run_before_main - run before main function start
  */
 
-void __attribute__ ((constructor)) run_before_main()
+void __attribute__ ((constructor)) run_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
