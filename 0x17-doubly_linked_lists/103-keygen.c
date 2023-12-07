65951bbdbf3e80876134dae4e4ec460626ad24f4
Lillymuller <eleltaalemu@gmail.com>
2023-12-07 05:11:40 -0800

    103-keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,45 +9,23 @@
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	char password[7], *codes;
-	int len = strlen(argv[1]), i, new;
+if (argc != 2)
+{
+printf("Usage: ./keygen5 username\n");
+return (1);
+}
 
-	codes = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char *username = argv[1];
+int key = 0;
+int i = 0;
+for (i = 0; i < strlen(username); i++)
+{
+key += username[i];
+}
 
-	new = (len ^ 59) & 63;
-	password[0] = codes[new];
+printf("Key: %d\n", key);
 
-	new = 0;
-	for (i = 0; i < len; i++)
-		new += argv[1][i];
-	password[1] = codes[(new ^ 79) & 63];
-
-	new = 1;
-	for (i = 0; i < len; i++)
-		new *= argv[1][i];
-	password[2] = codes[(new ^ 85) & 63];
-
-	new = 0;
-	for (i = 0; i < len; i++)
-	{
-		if (argv[1][i] > new)
-			new = argv[1][i];
-	}
-	srand(new ^ 14);
-	password[3] = codes[rand() & 63];
-
-	new = 0;
-	for (i = 0; i < len; i++)
-		new += (argv[1][i] * argv[1][i]);
-	password[4] = codes[(new ^ 239) & 63];
-
-	for (i = 0; i < argv[1][0]; i++)
-		new = rand();
-	password[5] = new[(tmp ^ 229) & 63];
-
-	password[6] = '\0';
-	printf("%s", password);
-	return (0);
+return (0);
 }
