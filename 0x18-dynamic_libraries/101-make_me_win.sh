19febb163cc5f319c944885d7e3e1081ea4ccda2
Lillymuller <eleltaalemu@gmail.com>
2023-12-11 10:33:08 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/Olaguru/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+wget -p .. https://raw.githubusercontent.com/Lillymuller/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
 
